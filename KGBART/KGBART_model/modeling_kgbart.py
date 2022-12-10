2ab81467bdf1f7816cb113695eb7a39d4267939e
何琦 <qi.he@qdtech.ai>
2022-12-10 10:36:31 +0800

    增加对比学习

--- KGBART/KGBART_model/modeling_kgbart.py
+++ KGBART/KGBART_model/modeling_kgbart.py
@@ -1755,13 +1755,13 @@ class KGBartForConditionalGeneration(PretrainedBartModel):
 
         # # 计算encoder output contrastive_loss
         encoder_states = encoder_last_hidden_states[torch.cumsum(word_subword, 1)]
-        subword = torch.cumsum(word_subword, 1) - torch.ones(word_subword.size(), dtype=torch.int64).cuda()
-        encoder_states = torch.cat([torch.index_select(j, 0, i).unsqueeze(0)
-                   for j, i in zip(encoder_last_hidden_states, torch.cumsum(subword, 1))])
-        norm_rep = encoder_states / encoder_states.norm(dim=2, keepdim=True)
-        encoder_output_cosine = torch.matmul(norm_rep, norm_rep.transpose(1, 2))
-        encoder_output_contrastive_loss = contrastive_loss(0.5, encoder_output_cosine, word_subword, 0, 0)
-        loss += encoder_output_contrastive_loss
+        # subword = torch.cumsum(word_subword, 1) - torch.ones(word_subword.size(), dtype=torch.int64).cuda()
+        # encoder_states = torch.cat([torch.index_select(j, 0, i).unsqueeze(0)
+        #            for j, i in zip(encoder_last_hidden_states, torch.cumsum(subword, 1))])
+        # norm_rep = encoder_states / encoder_states.norm(dim=2, keepdim=True)
+        # encoder_output_cosine = torch.matmul(norm_rep, norm_rep.transpose(1, 2))
+        # encoder_output_contrastive_loss = contrastive_loss(0.5, encoder_output_cosine, word_subword, 0, 0)
+        # loss += encoder_output_contrastive_loss
 
         if return_tuple:
             output = (lm_logits,) + outputs[1:]
